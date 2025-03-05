4e9da6810c5a1526b780a9571e7f0bf1b37cbeb1
Ivan Moskalev <ivan.moskalev@gmail.com>
2025-03-05 17:05:44 +0500

    Simplification of AttributedStringView

--- Sources/Chiaroscuro/AttributedStringView.swift
+++ Sources/Chiaroscuro/AttributedStringView.swift
@@ -13,10 +13,10 @@ public struct AttributedStringView: UIViewRepresentable {
         self.attributedText = attributedText
     }
 
-    public typealias UIViewType = CachingTextView
+    public typealias UIViewType = UITextView
 
-    public func makeUIView(context _: Context) -> CachingTextView {
-        let textView = CachingTextView()
+    public func makeUIView(context _: Context) -> UITextView {
+        let textView = UITextView()
         textView.isScrollEnabled = false
         textView.isEditable = false
         textView.attributedText = attributedText
@@ -29,40 +29,9 @@ public struct AttributedStringView: UIViewRepresentable {
         return textView
     }
 
-    public func updateUIView(_ textView: CachingTextView, context _: Context) {
+    public func updateUIView(_ textView: UITextView, context _: Context) {
         if !textView.attributedText.isEqual(to: attributedText) {
             textView.attributedText = attributedText
-        }
-    }
-}
-
-/// UITextView subclass that caches intrinsic content size.
-public class CachingTextView: UITextView {
-    private var cachedIntrinsicContentSize: CGSize?
-    private var lastBoundsWidth: CGFloat = 0
-
-    override public var frame: CGRect {
-        didSet {
-            if frame.width != oldValue.width {
-                invalidateIntrinsicContentSize()
-            }
-        }
-    }
-
-    override public var intrinsicContentSize: CGSize {
-        if lastBoundsWidth != bounds.width || cachedIntrinsicContentSize == nil {
-            lastBoundsWidth = bounds.width
-            let size = sizeThatFits(CGSize(width: bounds.width, height: .greatestFiniteMagnitude))
-            cachedIntrinsicContentSize = CGSize(width: size.width, height: size.height)
-        }
-        return cachedIntrinsicContentSize!
-    }
-
-    override public var attributedText: NSAttributedString! {
-        didSet {
-            if !oldValue.isEqual(to: attributedText) {
-                cachedIntrinsicContentSize = nil
-            }
         }
     }
 }
