9deec0aeee5caf1e045a411888b3f85270bb89fe
Ivan Moskalev <ivan.moskalev@gmail.com>
2025-04-22 14:21:59 +0500

    Revert "Make indicator compatible with iOS 16"
    This reverts commit 35bb93aa99eeb9174734d3262e15e456de59cd9b.

--- Sources/ChiaroscuroSyncIndicator/SyncStatusObserver.swift
+++ Sources/ChiaroscuroSyncIndicator/SyncStatusObserver.swift
@@ -3,6 +3,7 @@ import SwiftData
 import Combine
 import CloudKit
 import CoreData
+import Observation
 
 public enum SyncState: Equatable {
     case idle
@@ -26,29 +27,36 @@ public enum SyncState: Equatable {
     }
 }
 
-public final class SyncStatusObserver: ObservableObject {
-    @Published public private(set) var state: SyncState = .idle
-    private var cancellables = Set<AnyCancellable>()
+@Observable
+public final class SyncStatusObserver {
+    public var state: SyncState = .idle
+    @ObservationIgnored private var cancellables = Set<AnyCancellable>()
     
     public init() {
+        NotificationCenter.default.publisher(for: ModelContext.didSave)
+            .sink { [weak self] _ in
+                self?.state = .syncing
+            }
+            .store(in: &cancellables)
+        
         NotificationCenter.default.publisher(for: NSPersistentCloudKitContainer.eventChangedNotification)
-            .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
+            .debounce(for: 0.5, scheduler: RunLoop.main)
             .sink { [weak self] notification in
-                guard
-                    let event = notification.userInfo?[NSPersistentCloudKitContainer.eventNotificationUserInfoKey]
-                        as? NSPersistentCloudKitContainer.Event
-                else {
+                guard let event = notification.userInfo?[NSPersistentCloudKitContainer.eventNotificationUserInfoKey] as? NSPersistentCloudKitContainer.Event else {
                     return
                 }
                 
                 switch event.type {
                 case .setup, .import, .export:
-                    if event.endDate == nil {
+                    switch event.endDate {
+                    case .none:
                         self?.state = .syncing
-                    } else if event.succeeded {
-                        self?.state = .success(date: Date())
-                    } else if let error = event.error {
-                        self?.state = .error(error)
+                    case .some:
+                        if event.succeeded {
+                            self?.state = .success(date: Date())
+                        } else if let error = event.error {
+                            self?.state = .error(error)
+                        }
                     }
                 default:
                     break
